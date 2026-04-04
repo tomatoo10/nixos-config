4de4c512373aa3176fa3e215ead2d3af697ba25d
ak4m3 <>
2026-04-04 20:26:40 -0300

    adding noctalia-shell for sora

--- nixos/nix.nix
+++ nixos/nix.nix
@@ -37,9 +37,11 @@ in {
         "https://hyprland.cachix.org"
         "https://nix-community.cachix.org"
         "https://numtide.cachix.org"
+        "https://noctalia.cachix.org"
       ];
       trusted-substituters = ["https://hyprland.cachix.org"];
       trusted-public-keys = [
+        "noctalia.cachix.org-1:pCOR47nnMEo5thcxNDtzWpOxNFQsBRglJzxWPp3dkU4="
         "hyprland.cachix.org-1:a7pgxzMz7+chwVL3/pzj6jIBMioiJM7ypFP8PwtkuGc="
         "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
         "numtide.cachix.org-1:2ps1kLBUWjxIneOy1Ik6cQjb41X0iXVXeHigGmycPPE="
